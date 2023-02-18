6dac261676bc50ef17c64a8a8f1fab9e34eb9a86
666Abner666 <82553994+666Abner666@users.noreply.github.com>
2023-02-18 12:12:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// gapi.load('auth2', function () {
+//     gapi.auth2.init({
+//         client_id: '1097504830403-uujkl2ue0n6ok3tchisr66ll0pq02cge.apps.googleusercontent.com',
+//     });
+// });
+
+function handleCredentialResponse(googleUser) {
+    console.log('1')
+    console.log("Encoded JWT ID token:" + googleUser.credential);
+    // var userObject = jwt_decode(googleUser.credential);
+    // console.log(userObject)
+    // setUser(userObject)
+    // document.getElementById("signInDiv").hidden = true;
+    
+    // console.log(googleUser)
+    // var profile = googleUser.getBasicProfile();
+    // var name = profile.getName();
+    // var imageUrl = profile.getImageUrl();
+    // // 在页面上显示用户信息
+    // console.log(profile)
+    // console.log(name)
+    // console.log(imageUrl)
+
+    // console.log(profile.name)
+    // console.log(profile.imageUrl)
+
+    // document.getElementById('username').innerHTML = name;
+    // document.getElementById('userimage').src = imageUrl;
+}
